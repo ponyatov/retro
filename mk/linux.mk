36f1e66f0ab72ddddd5dee743093410e014c476b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-15 13:26:31 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/linux.mk
+++ mk/linux.mk
@@ -2,8 +2,12 @@
 binutils:
 gcc:
 
+LIBCC += --prefix $(CROSS) --disable-shared
+
 gmp: cross/lib/libgmp.a
 cross/lib/libgmp.a: $(REF)/$(GMP)/README
+	rm -rf $(TMP)/$(GMP) ; mkdir $(TMP)/$(GMP) ; cd $(TMP)/$(GMP) ;\
+	$(REF)/$(GMP)/configure $(LIBCC) && $(MAKE) install
 $(REF)/$(GMP)/README: $(DISTR)/$(GMP_GZ)
 	cd $(REF) ; xzcat $< | tar x && touch $@
 
